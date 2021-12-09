a288ecc4b587e6b3434db1592c561a136ba67097
Katherine Winters <goble.katherine@gmail.com>
2021-12-08 18:11:00 -0600

    inital commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionStatement2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+            
+            switch (subject.ToLower())
+
+            {
+                case "english":
+                    Console.WriteLine("Your favorite school subject is english.");
+                    break;
+                case "science":
+                    Console.WriteLine("Your favorite school subject is science.");
+                    break;
+                case "math":
+                    Console.WriteLine("Your favorite school subject is math.");
+                    break;
+                case "spanish":
+                    Console.WriteLine("Your favorite school subject is spanish.");
+                        break;
+                case "gym":
+                    Console.WriteLine("Your favorite school subject is gym.");
+                    break;
+                default:
+                    Console.WriteLine($"Your favorite school subject isn't an option.");
+                    break;
+            }
+
+        }
+    }
+}
